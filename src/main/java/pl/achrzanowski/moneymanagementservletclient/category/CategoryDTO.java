14b7cdb7dac2a2186388355514900aa6d61853de
aleksander.chrzanowski.dev <aleksander.chrzanowski.dev@gmail.com>
2022-08-12 13:56:00 +0200

    DTOs added

---
+++ src/main/java/pl/achrzanowski/moneymanagementservletclient/category/CategoryDTO.java
@@ -0,0 +1,10 @@
+package pl.achrzanowski.moneymanagementservletclient.category;
+
+import lombok.Data;
+
+@Data
+public class CategoryDTO {
+
+    private String name;
+
+}
---
+++ src/main/java/pl/achrzanowski/moneymanagementservletclient/importance/ImportanceDTO.java
@@ -0,0 +1,11 @@
+package pl.achrzanowski.moneymanagementservletclient.importance;
+
+import lombok.Data;
+
+@Data
+public class ImportanceDTO {
+
+    private String name;
+
+}
+
