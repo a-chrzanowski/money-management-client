2515133899527925ea6b58b55255cc5dab0d9a6b
aleksander.chrzanowski.dev <aleksander.chrzanowski.dev@gmail.com>
2022-08-12 13:41:39 +0200

    init commit

---
+++ src/main/java/pl/achrzanowski/moneymanagementservletclient/MoneyManagementServletClientApplication.java
@@ -0,0 +1,13 @@
+package pl.achrzanowski.moneymanagementservletclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementServletClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagementServletClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/achrzanowski/moneymanagementservletclient/MoneyManagementServletClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.achrzanowski.moneymanagementservletclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagementServletClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
